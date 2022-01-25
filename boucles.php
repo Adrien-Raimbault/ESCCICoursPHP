fa35f283a79e7df7bd9b53c61c080a007062f5fb
Adrien RAIMBAULT <adrien-raimbault@protonmail.com>
2022-01-25 16:15:23 +0100

    boucle for

---
+++ boucles.php
@@ -0,0 +1,18 @@
+<?php
+
+// Faire une boucle
+
+$resultat = "<ul>";
+
+for ($i=0 ; $i <100; $i++) {
+    if ($i % 3 == 0) {
+        $resultat .= "<li>";
+        $resultat .= $i;
+        $resultat .="</li>";
+    }
+}
+
+$resultat .= "</ul>";
+
+
+echo $resultat;
