b6e41fc2a5933c0fe6a0fffed6a2e4ee55f93c36
Adrien RAIMBAULT <adrien-raimbault@protonmail.com>
2022-01-25 16:23:52 +0100

    do...while

--- boucles.php
+++ boucles.php
@@ -4,7 +4,12 @@
 
 $toto = 0;
 
-while ($toto < 100 ){
-    echo $toto ."<br />";
-    $toto ++;
-}
+// while ($toto < 100 ){
+//     echo $toto ."<br />";
+//     $toto ++;
+// }
+
+do {
+    echo $toto;
+    $toto++;
+} while ($toto < 100);
