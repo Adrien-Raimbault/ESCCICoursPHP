a668459dfda7e4713874b082ba85729f5d5a9950
Adrien RAIMBAULT <adrien-raimbault@protonmail.com>
2022-01-25 14:47:27 +0100

    structures de contrôle if/else

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$valeur = "Michel";
+$resultat = "";
+
+if (is_numeric($valeur)) {
+    if ($valeur > 0) {
+        $resultat= "Positif";
+    }
+    elseif ($valeur < 0) {
+        $resultat= "Negatif";
+    }
+    else $resultat= "Nul";
+}
+else $resultat = "$valeur n'est pas un nombre";
+
+echo 
