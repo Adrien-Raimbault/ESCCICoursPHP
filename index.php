b7df4c6adf6d7b94d3e1d2c8440aa771a9f3f091
Adrien RAIMBAULT <adrien-raimbault@protonmail.com>
2022-01-25 15:30:13 +0100

    Ajout d'un lien vers Boucles

--- index.php
+++ index.php
@@ -1,17 +1,43 @@
 <?php
 
-$valeur = -10;
-$resultat = "";
+// $valeur = -2;
+// $resultat = "";
+
+// if (is_numeric($valeur)) {
+//     switch($valeur) {
+//         case ($valeur > O):
+//             $resultat = "positif";
+//             break;
+//         case ($valeur < O):
+//             $resultat = "négatif";
+//             break;
+//         default: $resultat = "nul";
+//     }
+// }
+// else $resultat = "$valeur n'est pas un nombre";
+
+// echo $resultat;
+
+$valeur = 34;
+$resultat= " ";
 
 if (is_numeric($valeur)) {
-    if ($valeur > 0) {
-        $resultat= "Positif";
+    switch($valeur) {
+        case ($valeur > 0):
+            $resultat = "positive";
+            break;
+        case ($valeur < 0) :
+            $resultat = "negative";
+            break;
+            default:
+            $resultat = "nulle";
     }
-    elseif ($valeur < 0) {
-        $resultat= "Negatif";
-    }
-    else $resultat= "Nul";
 }
-else $resultat = "$valeur n'est pas un nombre";
+else { 
+    $resultat = "non numerique";
+}
 
-echo $resultat;
+echo "La valeur est $resultat.";
+?>
+
+<p><a href="boucles.php">Boucles</a></p>
